09b529d3a519a30bdb46c5fe18ea5c782429cdd5
Nikos <nikos.sklikas@canonical.com>
2024-01-22 10:41:53 +0200

    fix: introduce noop logging and monitoring

---
+++ internal/monitoring/noop.go
@@ -0,0 +1,36 @@
+package monitoring
+
+import (
+	"github.com/canonical/identity-platform-admin-ui/internal/logging"
+)
+
+type NoopMonitor struct {
+	service string
+
+	logger logging.LoggerInterface
+}
+
+type NoopMetricInterface struct{}
+
+func (m *NoopMetricInterface) Observe(float64) {}
+
+func NewNoopMonitor(service string, logger logging.LoggerInterface) *NoopMonitor {
+	m := new(NoopMonitor)
+	m.service = service
+	m.logger = logger
+	return m
+}
+
+func (m *NoopMonitor) GetService() string {
+	return m.service
+}
+func (m *NoopMonitor) SetResponseTimeMetric(map[string]string, float64) error {
+	return nil
+}
+func (m *NoopMonitor) SetDependencyAvailability(map[string]string, float64) error {
+	return nil
+}
+
+func (m *NoopMonitor) GetResponseTimeMetric(tags map[string]string) (MetricInterface, error) {
+	return new(NoopMetricInterface), nil
+}

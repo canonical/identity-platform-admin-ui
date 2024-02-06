251a8a1b0be4935063f9e49927b06d8291c7d985
shipperizer <alexcabb@gmail.com>
2024-02-06 11:02:48 +0000

    fix: update noop openfga client with newer methods

--- internal/openfga/noop.go
+++ internal/openfga/noop.go
@@ -8,8 +8,10 @@ import (
 
 	"github.com/canonical/identity-platform-admin-ui/internal/logging"
 	"github.com/canonical/identity-platform-admin-ui/internal/monitoring"
+
 	"github.com/canonical/identity-platform-admin-ui/internal/tracing"
 	openfga "github.com/openfga/go-sdk"
+	"github.com/openfga/go-sdk/client"
 )
 
 type NoopClient struct {
@@ -26,19 +28,31 @@ func NewNoopClient(tracer tracing.TracingInterface, monitor monitoring.MonitorIn
 	return c
 }
 
-func (c *NoopClient) ListObjects(ctx context.Context, user string, relation string, objectType string) ([]string, error) {
+func (c *NoopClient) ListObjects(ctx context.Context, user, relation, objectType string) ([]string, error) {
 	return make([]string, 0), nil
 }
 
-func (c *NoopClient) Check(ctx context.Context, user string, relation string, object string) (bool, error) {
+func (c *NoopClient) Check(ctx context.Context, user, relation, object string) (bool, error) {
 	return true, nil
 }
 
-func (c *NoopClient) WriteTuple(ctx context.Context, user string, relation string, object string) error {
+func (c *NoopClient) BatchCheck(ctx context.Context, tuples ...Tuple) (bool, error) {
+	return true, nil
+}
+
+func (c *NoopClient) WriteTuple(ctx context.Context, user, relation, object string) error {
 	return nil
 }
 
-func (c *NoopClient) DeleteTuple(ctx context.Context, user string, relation string, object string) error {
+func (c *NoopClient) WriteTuples(ctx context.Context, tuples ...Tuple) error {
+	return nil
+}
+
+func (c *NoopClient) DeleteTuple(ctx context.Context, user, relation, object string) error {
+	return nil
+}
+
+func (c *NoopClient) DeleteTuples(ctx context.Context, tuples ...Tuple) error {
 	return nil
 }
 
@@ -52,4 +66,8 @@ func (c *NoopClient) WriteModel(ctx context.Context, model []byte) (string, erro
 
 func (c *NoopClient) CompareModel(ctx context.Context, model openfga.AuthorizationModel) (bool, error) {
 	return true, nil
+}
+
+func (c *NoopClient) ReadTuples(ctx context.Context, user, relation, object, continuationToken string) (*client.ClientReadResponse, error) {
+	return nil, nil
 }

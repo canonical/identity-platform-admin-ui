12830d934acf789af2c32b49b7b1f11175f0c1f0
barco <marco.basile@canonical.com>
2024-02-20 17:59:58 +0100

    feat: extend pull-spec.sh to allow for use of local file path

--- rebac-admin-backend/script/pull-spec.sh
+++ rebac-admin-backend/script/pull-spec.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env sh
 
-# Pull latest stable OpenAPI spec from GitHub and auto-generate Go types.
+# Generate Go server and types from an OpenAPI spec definition file.
+# A local file path can be passed as the first parameter to the script.
 #
-# This command should be run from the Makefile's parent directory.
+# If no parameter is passed, the OpenAPI spec will be downloaded from
+# the Github repository canonical/openfga-admin-openapi-spec.
+#
+# This script should be run from the Makefile's parent directory.
 
 set -e
 
-_tmpdir=/tmp/openfga-admin-openapi-spec
-cleanup() {
-  rm -rf "$_tmpdir"
-}
-trap cleanup 0
+OPENAPI_SPEC_FILE="$1"
 
-cleanup
-mkdir -p "$_tmpdir"
-git clone -q --depth=1 git@github.com:canonical/openfga-admin-openapi-spec "$_tmpdir"
+if [ -z "$OPENAPI_SPEC_FILE" ]; then
+  _tmpdir=/tmp/openfga-admin-openapi-spec
+
+  cleanup() {
+    rm -rf "$_tmpdir"
+  }
+  trap cleanup 0
+
+  cleanup
+
+  mkdir -p "$_tmpdir"
+  git clone -q --depth=1 git@github.com:canonical/openfga-admin-openapi-spec "$_tmpdir"
+  OPENAPI_SPEC_FILE="${_tmpdir}/openapi.yaml"
+
+elif ! [ -r "$OPENAPI_SPEC_FILE" ]; then
+  echo "Can't read the '$OPENAPI_SPEC_FILE', exiting";
+  exit 1
+fi
 
 go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest
-oapi-codegen -generate types,spec -package resources "${_tmpdir}/openapi.yaml" > v1/resources/generated_types.go
-oapi-codegen -generate chi-server -package resources "${_tmpdir}/openapi.yaml" > v1/resources/generated_server.go
+oapi-codegen -generate types,spec -package resources "$OPENAPI_SPEC_FILE" > v1/resources/generated_types.go
+oapi-codegen -generate chi-server -package resources "$OPENAPI_SPEC_FILE" > v1/resources/generated_server.go

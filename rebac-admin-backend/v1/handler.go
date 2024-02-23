9cd297e9f253a800b1c718a50a367395d9202b49
barco <marco.basile@canonical.com>
2024-02-23 14:58:46 +0100

    refactoring: service renamed to handler to reflect the controller handler responsibilies
    fixup! refactoring: service renamed to handler to reflect the controller handler responsibilies

---
+++ rebac-admin-backend/v1/handler.go
@@ -0,0 +1,21 @@
+// Copyright 2024 Canonical Ltd.
+
+package v1
+
+import (
+	"github.com/canonical/identity-platform-admin-ui/rebac-admin-backend/v1/interfaces"
+	"github.com/canonical/identity-platform-admin-ui/rebac-admin-backend/v1/resources"
+)
+
+type handler struct {
+	// TODO(CSS-7311): the Unimplemented struct should be removed from here after all
+	// endpoints are implemented.
+	resources.Unimplemented
+
+	Groups              GroupsServiceBackend
+	GroupsAuthorization GroupsAuthorizationBackend
+
+	Identities              interfaces.IdentitiesService
+	IdentitiesAuthorization interfaces.IdentitiesAuthorization
+	IdentitiesErrorMapper   ErrorResponseMapper
+}

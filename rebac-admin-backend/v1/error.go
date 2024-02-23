a0381aafedb0a8a99ba3d9c07bb6ae407cbe30aa
barco <marco.basile@canonical.com>
2024-02-23 14:52:22 +0100

    feat: introduce `ErrorResponseMapper` interface and basic delegate implementation

--- rebac-admin-backend/v1/error.go
+++ rebac-admin-backend/v1/error.go
@@ -4,6 +4,8 @@ package v1
 
 import (
 	"fmt"
+
+	"github.com/canonical/identity-platform-admin-ui/rebac-admin-backend/v1/resources"
 )
 
 // baseError struct for all error structs
@@ -27,4 +29,36 @@ type NotFoundError struct {
 
 func (e *NotFoundError) Error() string {
 	return fmt.Sprintf("Not found: %s", e.message)
+}
+
+// ErrorResponseMapper is the basic interface to allow for error -> http response mapping
+type ErrorResponseMapper interface {
+	MapError(error) *resources.Response
+}
+
+type delegateErrorResponseMapper struct {
+	delegate ErrorResponseMapper
+}
+
+func (d delegateErrorResponseMapper) MapError(err error) *resources.Response {
+	var response *resources.Response
+	if d.delegate != nil {
+		response = d.delegate.MapError(err)
+	}
+
+	if nil == response {
+		response = mapErrorResponse(err)
+	}
+
+	return response
+}
+
+func NewDefaultErrorResponseMapper() ErrorResponseMapper {
+	return &delegateErrorResponseMapper{}
+}
+
+// NewDelegateErrorResponseMapper creates an error mapper that relies on the default error mapping
+// only if the provided error mapper returns nil for the error used as input
+func NewDelegateErrorResponseMapper(m ErrorResponseMapper) ErrorResponseMapper {
+	return &delegateErrorResponseMapper{delegate: m}
 }

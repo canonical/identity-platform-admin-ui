623940b14b43133a3c9ee8dfbb0097ff0cb20318
barco <marco.basile@canonical.com>
2024-02-23 14:48:06 +0100

    refactor: add message field to exported errors

--- rebac-admin-backend/v1/error.go
+++ rebac-admin-backend/v1/error.go
@@ -2,16 +2,29 @@
 
 package v1
 
+import (
+	"fmt"
+)
+
+// baseError struct for all error structs
+type baseError struct {
+	message string
+}
+
 // UnauthorizedError represents unauthorized access error.
-type UnauthorizedError struct{}
+type UnauthorizedError struct {
+	baseError
+}
 
 func (e *UnauthorizedError) Error() string {
-	return "unauthorized"
+	return fmt.Sprintf("Unauthorized: %s", e.message)
 }
 
 // NotFoundError represents missing entity error.
-type NotFoundError struct{}
+type NotFoundError struct {
+	baseError
+}
 
 func (e *NotFoundError) Error() string {
-	return "not found"
+	return fmt.Sprintf("Not found: %s", e.message)
 }

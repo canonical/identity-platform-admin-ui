1565e45bb78b3d23efb1d84dc0c2b423a458bf86
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-03-07 23:40:09 +0000

    feat: add `/swagger.json` endpoint handler
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

---
+++ rebac-admin-backend/v1/swagger.go
@@ -0,0 +1,28 @@
+// Copyright 2024 Canonical Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	"net/http"
+
+	"github.com/canonical/identity-platform-admin-ui/rebac-admin-backend/v1/resources"
+)
+
+// Returns the OpenAPI spec as a JSON file.
+// (GET /swagger.json)
+func (h handler) SwaggerJson(w http.ResponseWriter, req *http.Request) {
+	swagger, err := resources.GetSwagger()
+	if err != nil {
+		writeErrorResponse(w, NewUnknownError("cannot retrieve swagger instance"))
+		return
+	}
+
+	body, err := swagger.MarshalJSON()
+	if err != nil {
+		writeErrorResponse(w, NewUnknownError("cannot marshal spec as JSON"))
+		return
+	}
+
+	w.Write(body)
+}

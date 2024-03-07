c0b87e347b509b9d019988e9fce98383e80b1fe6
barco <marco.basile@canonical.com>
2024-03-07 11:39:19 +0100

    feat: add Capabilities interface

---
+++ rebac-admin-backend/v1/interfaces/capabilities.go
@@ -0,0 +1,17 @@
+// Copyright 2024 Canonical Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package interfaces
+
+import (
+	"context"
+
+	"github.com/canonical/identity-platform-admin-ui/rebac-admin-backend/v1/resources"
+)
+
+// CapabilitiesService defines an abstract backend to handle capabilities related operations.
+type CapabilitiesService interface {
+	ListCapabilities(ctx context.Context) (*resources.Capabilities, error)
+}
+
+type CapabilitiesAuthorization interface{}

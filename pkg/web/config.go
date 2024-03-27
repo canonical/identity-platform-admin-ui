a660066d58cce54b8e18a2968a9a6ce7bc0cd25d
shipperizer <alexcabb@gmail.com>
2024-03-27 13:42:28 +0100

    fix: bundle up external clients and o11y setup into config structs

---
+++ pkg/web/config.go
@@ -0,0 +1,106 @@
+// Copyright 2024 Canonical Ltd
+// SPDX-License-Identifier: AGPL
+
+package web
+
+import (
+	trace "go.opentelemetry.io/otel/trace"
+
+	ih "github.com/canonical/identity-platform-admin-ui/internal/hydra"
+	ik "github.com/canonical/identity-platform-admin-ui/internal/kratos"
+	"github.com/canonical/identity-platform-admin-ui/internal/logging"
+	"github.com/canonical/identity-platform-admin-ui/internal/monitoring"
+	io "github.com/canonical/identity-platform-admin-ui/internal/oathkeeper"
+)
+
+// O11yConfig is a wrapper config for all the observability objects
+type O11yConfig struct {
+	tracer  trace.Tracer
+	monitor monitoring.MonitorInterface
+	logger  logging.LoggerInterface
+}
+
+// Tracer returns the tracing object
+func (c *O11yConfig) Tracer() trace.Tracer {
+	return c.tracer
+}
+
+// Monitor returns a monitor object
+func (c *O11yConfig) Monitor() monitoring.MonitorInterface {
+	return c.monitor
+}
+
+// Logger returns a logger object
+func (c *O11yConfig) Logger() logging.LoggerInterface {
+	return c.logger
+}
+
+// NewO11yConfig create an observability config object with a monitor, logger and tracer
+func NewO11yConfig(tracer trace.Tracer, monitor monitoring.MonitorInterface, logger logging.LoggerInterface) *O11yConfig {
+	c := new(O11yConfig)
+
+	c.tracer = tracer
+	c.monitor = monitor
+	c.logger = logger
+
+	return c
+}
+
+// ExternalClientsConfig is a wrapper config for all the third party clients
+type ExternalClientsConfig struct {
+	hydraAdmin       *ih.Client
+	kratosAdmin      *ik.Client
+	kratosPublic     *ik.Client
+	oathkeeperPublic *io.Client
+	ofga             OpenFGAClientInterface
+	authorizer       OpenFGAClientInterface
+}
+
+// HydraAdmin returns an hydra client to interact with the admin API
+func (c *ExternalClientsConfig) HydraAdmin() *ih.Client {
+	return c.hydraAdmin
+}
+
+// KratosAdmin returns a kratos client to interact with the admin API
+func (c *ExternalClientsConfig) KratosAdmin() *ik.Client {
+	return c.kratosAdmin
+}
+
+// KratosPublic returns a kratos client to interact with the public API
+func (c *ExternalClientsConfig) KratosPublic() *ik.Client {
+	return c.kratosPublic
+}
+
+// OathkeeperPublic returns an oathkeeper client to interact with the public API
+func (c *ExternalClientsConfig) OathkeeperPublic() *io.Client {
+	return c.oathkeeperPublic
+}
+
+// OpenFGA returns an openfga client
+func (c *ExternalClientsConfig) OpenFGA() OpenFGAClientInterface {
+	return c.ofga
+}
+
+// Authorizer returns an openfga client used for the authorization middleware
+func (c *ExternalClientsConfig) Authorizer() OpenFGAClientInterface {
+	return c.authorizer
+}
+
+// SetAuthorizer sets the authorization middleware
+func (c *ExternalClientsConfig) SetAuthorizer(o OpenFGAClientInterface) {
+	c.authorizer = o
+}
+
+// NewExternalClientsConfig create a third party config object for all the external clients needed
+func NewExternalClientsConfig(hydra *ih.Client, kratosAdmin *ik.Client, kratosPublic *ik.Client, oathkeeper *io.Client, ofga OpenFGAClientInterface, authorizer OpenFGAClientInterface) *ExternalClientsConfig {
+	c := new(ExternalClientsConfig)
+
+	c.hydraAdmin = hydra
+	c.kratosAdmin = kratosAdmin
+	c.kratosPublic = kratosPublic
+	c.oathkeeperPublic = oathkeeper
+	c.ofga = ofga
+	c.authorizer = authorizer
+
+	return c
+}

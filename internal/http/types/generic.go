074a5c79ab921881b12c6dcde06af423bc020169
shipperizer <alexcabb@gmail.com>
2024-04-05 18:04:58 +0200

    fix: add page tokens to the response

--- internal/http/types/generic.go
+++ internal/http/types/generic.go
@@ -16,14 +16,19 @@ type Response struct {
 }
 
 type Pagination struct {
-	Page int64 `json:"page"`
-	Size int64 `json:"size"`
+	Page      int64  `json:"page"`
+	PageToken string `json:"page_token"`
+	Size      int64  `json:"size"`
+
+	Next  string `json:"next,omitempty"`
+	Prev  string `json:"prev,omitempty"`
+	First string `json:"first,omitempty"`
 }
 
 func NewPaginationWithDefaults() *Pagination {
 	p := new(Pagination)
 
-	p.Page = 1
+	p.PageToken = ""
 	p.Size = 100
 
 	return p
@@ -33,12 +38,13 @@ func ParsePagination(q url.Values) *Pagination {
 
 	p := NewPaginationWithDefaults()
 
-	if page, err := strconv.ParseInt(q.Get("page"), 10, 64); err == nil && page > 1 {
-		p.Page = page
-	}
-
+	// TODO @shipperizer introduce go-playground/validator
 	if size, err := strconv.ParseInt(q.Get("size"), 10, 64); err == nil && size > 0 {
 		p.Size = size
+	}
+
+	if token := q.Get("page_token"); token != "" {
+		p.PageToken = token
 	}
 
 	return p

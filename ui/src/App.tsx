576c70ceb060dd62edcbf072afc48e4fbeed93b6
Huw Wilkins <huw.wilkins@canonical.com>
2024-05-13 16:34:12 +1000

    Change main layout to use application layout from react-components.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { FC, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { ApplicationLayout } from "@canonical/react-components";
 import { ReBACAdmin } from "@canonical/rebac-admin";
 import Loader from "components/Loader";
 import Login from "components/Login";
@@ -8,9 +9,21 @@ import NoMatch from "components/NoMatch";
 import ProviderList from "pages/providers/ProviderList";
 import IdentityList from "pages/identities/IdentityList";
 import SchemaList from "pages/schemas/SchemaList";
-import Navigation from "components/Navigation";
 import Panels from "components/Panels";
 import useLocalStorage from "util/useLocalStorage";
+import Logo from "components/Logo";
+import Navigation from "components/Navigation";
+
+const VITE_API_URL =
+  typeof import.meta.env.VITE_API_URL === "string"
+    ? import.meta.env.VITE_API_URL
+    : "/";
+
+const API_URL = (
+  VITE_API_URL.startsWith("http") || VITE_API_URL.startsWith("/")
+    ? VITE_API_URL
+    : "/"
+) as `${"http" | "/"}${string}`;
 
 const App: FC = () => {
   // Store a user token that will be passed to the API using the
@@ -21,48 +34,50 @@ const App: FC = () => {
     token: string;
   } | null>("user", null);
   return (
-    <div className="l-application" role="presentation">
-      <Navigation
-        username={authUser?.username}
-        logout={() => {
-          setAuthUser(null);
-          window.location.reload();
-        }}
-      />
-      <main className="l-main">
-        <Suspense fallback={<Loader />}>
-          <Routes>
+    <ApplicationLayout
+      aside={<Panels />}
+      logo={<Logo />}
+      sideNavigation={
+        <Navigation
+          username={authUser?.username}
+          logout={() => {
+            setAuthUser(null);
+            window.location.reload();
+          }}
+        />
+      }
+    >
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login isAuthenticated={!!authUser} setAuthUser={setAuthUser} />
+            }
+          >
             <Route
               path="/"
+              element={<Navigate to="/provider" replace={true} />}
+            />
+            <Route path="/provider" element={<ProviderList />} />
+            <Route path="/client" element={<ClientList />} />
+            <Route path="/identity" element={<IdentityList />} />
+            <Route path="/schema" element={<SchemaList />} />
+            <Route
+              path="/*"
               element={
-                <Login isAuthenticated={!!authUser} setAuthUser={setAuthUser} />
+                <ReBACAdmin
+                  apiURL={API_URL}
+                  asidePanelId="rebac-admin-panel"
+                  authToken={authUser?.token}
+                />
               }
-            >
-              <Route
-                path="/"
-                element={<Navigate to="/provider" replace={true} />}
-              />
-              <Route path="/provider" element={<ProviderList />} />
-              <Route path="/client" element={<ClientList />} />
-              <Route path="/identity" element={<IdentityList />} />
-              <Route path="/schema" element={<SchemaList />} />
-              <Route
-                path="/*"
-                element={
-                  <ReBACAdmin
-                    apiURL={import.meta.env.VITE_API_URL}
-                    asidePanelId="rebac-admin-panel"
-                    authToken={authUser?.token}
-                  />
-                }
-              />
-              <Route path="*" element={<NoMatch />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </main>
-      <Panels />
-    </div>
+            />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </ApplicationLayout>
   );
 };
 

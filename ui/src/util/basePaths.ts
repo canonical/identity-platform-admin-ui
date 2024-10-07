7e8f53098f5cf5c4e03218f8239f2e6378da8ee2
Huw Wilkins <huw.wilkins@canonical.com>
2024-10-08 09:14:04 +1100

    refactor: set UI base from template var instead of window location

--- ui/src/util/basePaths.ts
+++ ui/src/util/basePaths.ts
@@ -1,18 +1,25 @@
 import { removeTrailingSlash } from "util/removeTrailingSlash";
 type BasePath = `/${string}`;
 
+declare global {
+  interface Window {
+    base?: string;
+  }
+}
+
 export const calculateBasePath = (): BasePath => {
-  const path = window.location.pathname;
-  // find first occurrence of /ui/ and return the string before it
-  const basePath = path.match(/(.*\/ui(?:\/|$))/);
+  let basePath = "";
+  if ("base" in window && typeof window.base === "string") {
+    basePath = window.base;
+  }
   if (basePath) {
-    return `${removeTrailingSlash(basePath[0])}/` as BasePath;
+    return `${removeTrailingSlash(basePath)}/` as BasePath;
   }
   return "/";
 };
 
-export const basePath: BasePath = calculateBasePath();
-export const apiBasePath: BasePath = `${basePath}../api/v0/`;
+export const basePath: BasePath = `${calculateBasePath()}ui`;
+export const apiBasePath: BasePath = `${calculateBasePath()}api/v0/`;
 
 export const appendBasePath = (path: string) =>
   `${removeTrailingSlash(basePath)}/${path.replace(/^\//, "")}`;

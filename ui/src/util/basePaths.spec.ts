3a1e053913f922719c461f9e40ef80fb68ca8b0d
Huw Wilkins <huw.wilkins@canonical.com>
2024-10-10 13:50:41 +1100

    refactor: fetch basename from base tag

--- ui/src/util/basePaths.spec.ts
+++ ui/src/util/basePaths.spec.ts
@@ -5,34 +5,59 @@ import {
 } from "./basePaths";
 
 vi.mock("./basePaths", async () => {
-  window.base = "/example";
+  const basePath = "/example/ui";
+  // Can't use the setBase function here as this mock gets hoisted to the top of
+  // the file so gets executed before setBase exists.
+  const base = document.createElement("base");
+  base.setAttribute("href", basePath);
+  document.body.appendChild(base);
   const actual = await vi.importActual("./basePaths");
   return {
     ...actual,
-    basePath: "/example/ui/",
-    apiBasePath: "/example/api/v0/",
+    basePath,
   };
 });
 
+const cleanBase = () =>
+  document.querySelectorAll("base").forEach((base) => {
+    document.body.removeChild(base);
+  });
+
+const setBase = (href: string) => {
+  const base = document.createElement("base");
+  base.setAttribute("href", href);
+  document.body.appendChild(base);
+};
+
+// Clean up the base created by the mock above.
+cleanBase();
+
 describe("calculateBasePath", () => {
+  afterEach(() => {
+    cleanBase();
+  });
+
   it("resolves with ui path", () => {
-    window.base = "/test/";
+    setBase("/test/");
     const result = calculateBasePath();
     expect(result).toBe("/test/");
   });
 
   it("resolves with ui path without trailing slash", () => {
-    window.base = "/test";
+    setBase("/test");
     const result = calculateBasePath();
     expect(result).toBe("/test/");
   });
 
-  it("resolves with root path if the base is not provided", () => {
-    if (window.base) {
-      delete window.base;
-    }
+  //   it("handles full URL", () => {
+  //     setBase("http://example.com/test/");
+  //     const result = calculateBasePath();
+  //     expect(result).toBe("/test/");
+  //   });
+
+  it("resolves if the base is not provided", () => {
     const result = calculateBasePath();
-    expect(result).toBe("/");
+    expect(result).toBe("/ui/");
   });
 });
 
@@ -48,10 +73,10 @@ describe("appendBasePath", () => {
 
 describe("appendAPIBasePath", () => {
   it("handles paths with a leading slash", () => {
-    expect(appendAPIBasePath("/test")).toBe("/example/api/v0/test");
+    expect(appendAPIBasePath("/test")).toBe("/api/v0/test");
   });
 
   it("handles paths without a leading slash", () => {
-    expect(appendAPIBasePath("test")).toBe("/example/api/v0/test");
+    expect(appendAPIBasePath("test")).toBe("/api/v0/test");
   });
 });

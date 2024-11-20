ed4ebdabcae22eeb1be3b6cb25c481699c53c97a
Huw Wilkins <huw.wilkins@canonical.com>
2024-11-20 13:33:50 +1100

    chore(ci): add UI test coverage reporter

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -6,6 +6,12 @@ export default defineConfig((configEnv) =>
     viteConfig({ ...configEnv, mode: "development" }),
     defineConfig({
       test: {
+        coverage: {
+          exclude: ["src/test/**", "**/*.d.ts"],
+          include: ["src/**/*.[jt]s?(x)"],
+          reporter: ["text", "json-summary", "json", "cobertura"],
+          reportOnFailure: true,
+        },
         environment: "happy-dom",
         globals: true,
         include: ["./src/**/*.{test,spec}.{ts,tsx}"],

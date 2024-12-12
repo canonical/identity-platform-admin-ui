e23d79491bc74c4b86d13f742e23f3639166266f
Huw Wilkins <huw.wilkins@canonical.com>
2024-12-12 15:51:55 +1100

    chore(tests): add tests for root app

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,32 +1,3 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import "./sass/styles.scss";
-import { NotificationProvider } from "@canonical/react-components";
-import { basePath } from "util/basePaths";
-import { removeTrailingSlash } from "util/removeTrailingSlash";
+import { init } from "root";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-      // Cache queries for 30 seconds by default.
-      staleTime: 30000,
-    },
-  },
-});
-
-const rootElement = document.getElementById("app");
-if (!rootElement) throw new Error("Failed to find the root element");
-const root = createRoot(rootElement);
-root.render(
-  <Router basename={removeTrailingSlash(basePath)}>
-    <QueryClientProvider client={queryClient}>
-      <NotificationProvider>
-        <App />
-      </NotificationProvider>
-    </QueryClientProvider>
-  </Router>,
-);
+init();
